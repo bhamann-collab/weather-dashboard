a23104445d75dd8fc256933333d011220a6bace7
bhamann-collab <brockohamann@gmail.com>
2020-03-31 17:56:17 +1030

    Date are now working for the 5 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,38 @@
 $(document).ready(function () {
+
+
     //Getting the time on initial upload
     var date = new Date();
 
+    $("#search-bar").keyup(function(event) {
+      if (event.keyCode === 13) {
+          $("#search-button").click();
+      }
+  });
+
     //When clicking on the Search button, The city gets saved to the archive and the function that holds the AJAX call gets called
     $("#search-button").on('click', function() {
-        var cityInput = $("#search-bar").val();
-        updateCityStats(cityInput);
+      var addCity = true;
+      var cityInput = $("#search-bar").val();
+      updateCityStats(cityInput, addCity);
+    })
+
+    $("#previous-cities").on('click', '.city-archive' , function(){
+      console.log($(this).text())
+      var addCity = false;
+      var cityInput = $(this).text()
+      updateCityStats(cityInput, addCity);
     })
 
 
     //Function that holds the AJAX call to get the information of the weather for a chosen city. Once when the call is successful, we go to the main powerhouse function
-    function updateCityStats(cityName) {
+    function updateCityStats(cityName, addCity) {
         var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=635d437441ea04900582285c2e4d3a35&cnt=42`;
         $.ajax({
             url: queryURL,
             success: function (response) {
                 console.log(response)
-                updatePage(response)
+                updatePage(response, addCity)
             },
             error: function () {
                 console.log("Unable to identify location - OpenAPI");
@@ -40,15 +56,16 @@ $(document).ready(function () {
     }
 
     //This function is the main control source to change the page's content if there is an AJAX call
-    function updatePage(cityObject) {
+    function updatePage(cityObject, addCity) {
         weatherCurrent = cityObject.list[0].main;
         updateTitle(cityObject.city.name, cityObject.city.country, date);
         updateTemp(weatherCurrent.temp);
         updateHumidity(weatherCurrent.humidity);
         updateWindSpeed(cityObject.list[0].wind.speed)
         updateCityUV(cityObject)
-        fiveDayForecast(cityObject);
-        addCityToHistory();
+        fiveDayForecast(cityObject, date);
+        addCityToHistory(cityObject.city.name, addCity);
+        date = new Date();
     }
 
     //Updates the name, country and date of the current city being viewed
@@ -87,36 +104,57 @@ $(document).ready(function () {
     }
 
     //This function inserts the Temperature, the Humidity and the image for the the next five days of that particular city
-    function fiveDayForecast(city) {
+    function fiveDayForecast(city, time) {
+
+      var date = [];
+      var month = [];
+      var year = [];
+      for(var i = 0; i < 5; i++){
+        time.setDate(time.getDate() + 1)
+        date[i] = time.getDate()
+        month[i] = time.getMonth()
+        year[i] = time.getYear() + 1900;
+      }
+      console.log(date)
+
+
+
         
         $("#day-one").find(".temp").text(`Temp: ${(city.list[9].main.temp-273.15).toFixed(2)} C`);
         $("#day-one").find(".humid").text(`Humidity: ${city.list[9].main.humidity}%`);
+        $("#day-one").find(".date").text(`${date[0]}/${month[0]}/${year[0]}`)
         chooseImage(city.list[9].weather[0].main, $("#day-one"));
 
 
         $("#day-two").find(".temp").text(`Temp: ${(city.list[17].main.temp-273.15).toFixed(2)} C`);
         $("#day-two").find(".humid").text(`Humidity: ${city.list[17].main.humidity}%`);
+        $("#day-two").find(".date").text(`${date[1]}/${month[1]}/${year[1]}`)
         chooseImage(city.list[17].weather[0].main, $("#day-two"));
 
 
         $("#day-three").find(".temp").text(`Temp: ${(city.list[25].main.temp-273.15).toFixed(2)} C`);
         $("#day-three").find(".humid").text(`Humidity: ${city.list[25].main.humidity}%`);
+        $("#day-three").find(".date").text(`${date[2]}/${month[2]}/${year[2]}`)
         chooseImage(city.list[25].weather[0].main, $("#day-three"));
         
 
         $("#day-four").find(".temp").text(`Temp: ${(city.list[33].main.temp-273.15).toFixed(2)} C`);
         $("#day-four").find(".humid").text(`Humidity: ${city.list[33].main.humidity}%`);
+        $("#day-four").find(".date").text(`${date[3]}/${month[3]}/${year[3]}`)
         chooseImage(city.list[33].weather[0].main, $("#day-four"));
         
 
         $("#day-five").find(".temp").text(`Temp: ${(city.list[39].main.temp-273.15).toFixed(2)} C`);
         $("#day-five").find(".humid").text(`Humidity: ${city.list[39].main.humidity}%`);
+        $("#day-five").find(".date").text(`${date[4]}/${month[4]}/${year[4]}`)
         chooseImage(city.list[39].weather[0].main, $("#day-five"));
 
     }
 
-    function addCityToHistory() {
-        console.log("yeah")
+    function addCityToHistory(cityInput, addCity) {
+      if(addCity === true){
+        $('#previous-cities').prepend(`<li class="city-archive">${cityInput}</li>`);
+      }
     }
 
     //Chooses images to represent the current state of the weather. It will represent an image for Rain, Snow, Clouds and Clear
